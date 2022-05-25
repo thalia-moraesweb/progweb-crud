b39e010a2b0c8cb0396cc856f191f904c1e0bdaa
thalia-moraesweb <moraesthalia.silva@gmail.com>
2022-05-25 16:39:07 -0300

    Uso de Bootstrap 4.6

--- fabricantes/listar.php
+++ fabricantes/listar.php
@@ -8,9 +8,50 @@ $listaDeFabricantes = lerFabricantes($conexao);
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title> Fabricantes | SELECT - CRUD com PHP e MySQL </title>
-<link href="../css/style.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" 
+integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+<style>
+    /*Customização de uma classe do Bootstrap */
+ .btn-primary { background-color: darkblue; }
+</style>
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
 
 <div class="container">
     <h1>Fabricantes | SELECT -
@@ -21,11 +62,11 @@ $listaDeFabricantes = lerFabricantes($conexao);
 <div class="container">
     
     <h2>Lendo e carregando todos os fabricantes</h2>
-    <p><a href="inserir.php">Inserir</a></p>    
+    <p><a class="btn btn-primary btn-lg" href="inserir.php">Inserir</a></p>    
 
-    <table>
+    <table class="table table-striped table-info table-bordered">
         <caption> Lista de Fabricantes </caption>
-        <thead>
+        <thead class="thead-dark">
             <tr>
                 <th>ID</th>
                 <th>Nome</th>
@@ -40,8 +81,8 @@ $listaDeFabricantes = lerFabricantes($conexao);
         <td> <?=$fabricante["id"]?> </td>
         <td> <?=$fabricante["nome"]?> </td>
         <td>
-            <a href="atualizar.php?id=<?=$fabricante["id"]?>">Atualizar</a> -
-            <a href="excluir.php?id=<?=$fabricante["id"]?>">Excluir</a>
+            <a class="btn  btn-warning" href="atualizar.php?id=<?=$fabricante["id"]?>">Atualizar</a> -
+            <a class="btn  btn-danger" href="excluir.php?id=<?=$fabricante["id"]?>">Excluir</a>
         </td>
     </tr>
 <?php
@@ -55,7 +96,12 @@ require "../includes/desconecta.php"; //opcional algo não muito importante
  
 </div>
 
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" 
+integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+</script>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 
 </body>
 </html>
--- produtos/listar.php
+++ produtos/listar.php
@@ -8,7 +8,8 @@ $listaDeProdutos = lerProdutos($conexao);
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Produtos | SELECT - CRUD com PHP e MySQL </title>
-<link href="../css/style.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" 
+integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 </head>
 <body>
 
@@ -21,20 +22,23 @@ $listaDeProdutos = lerProdutos($conexao);
     
     <h2>Lendo e carregando todos os produtos</h2>
     <p><a href="inserir.php">Inserir</a></p>  
-
     <hr>
 
+    <div class="row">
 <?php foreach($listaDeProdutos as $produto ) {?>
-    <ul>
-        <li><b>Nome:</b> <?=$produto['produto']?> </li>
-        <li><b>Preço:</b> <?=formataMoeda($produto['preco'])?> </li>
-        <li><b>Quantidade:</b> <?=$produto['quantidade']?> </li>
-        <li><b>Descrição:</b> <?=$produto['descricao']?>  </li>
-        <li><b>Fabricante:</b> <?=$produto['fabricante']?> </li>
-    </ul>
+    <article class="col-sm-6 col-md-4">    <!-- representa uma media query deixando asim o site responsivo -->
+        <h3> <?=$produto['produto']?> </h3>
+        <p><b>Preço:</b> <?=formataMoeda($produto['preco'])?> </p>
+        <p><b>Quantidade:</b> <?=$produto['quantidade']?> </p>
+        <p><b>Descrição:</b> <?=$produto['descricao']?>  </p>
+        <p><b>Fabricante:</b> <?=$produto['fabricante']?> </p>
+    <p>
     <a href="atualizar.php?id=<?=$produto['id']?>">Atualizar</a>
     <a href="excluir.php?id=<?=$produto['id']?>">Excluir</a>
+    </p>
+</article>
 <?php } ?>
+</div>
 
 </div>
 
